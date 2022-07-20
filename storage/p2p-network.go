2f41e553c424da25ab9bff69d4f049989091a855
Uzama Zaid <uzamazaid@Uzamas-MacBook.local>
2022-07-20 14:12:20 +0530

    added comments, code improved in p2p network

--- storage/p2p-network.go
+++ storage/p2p-network.go
@@ -11,13 +11,23 @@ import (
 	"p2p-network-simulator/storage/tree"
 )
 
+// P2PNetwork: it represts the actual p2p network
 type P2PNetwork struct {
+	// the network topology contains list of trees
 	topology []*tree.Tree
-	treap    *treap.Treap
-	ids      map[int]struct{}
-	lock     sync.Mutex
+
+	// we use treap to store peers which has enough capacity (capacity > 0).
+	// we can easily identify the peer which has maximum capacity (the root node of the treap)
+	treap *treap.Treap
+
+	// keeps track of joint peer's id in a set data structure to ensure ids are unique
+	ids map[int]struct{}
+
+	// using mutex to prevent from the concurrent accesses to the network
+	lock sync.Mutex
 }
 
+// NewP2PNetwork: creates new p2p network
 func NewP2PNetwork() interfaces.P2PNetwork {
 	return &P2PNetwork{
 		topology: make([]*tree.Tree, 0),
@@ -26,28 +36,37 @@ func NewP2PNetwork() interfaces.P2PNetwork {
 	}
 }
 
+// Join: a new node joining the network
 func (network *P2PNetwork) Join(node entities.Node) error {
+	// using locks to prevent from concurrent access
 	network.lock.Lock()
 	defer network.lock.Unlock()
 
+	// check whether the given node id is already reserved
 	_, ok := network.ids[node.Id]
 	if ok {
 		return fmt.Errorf("id %d already reserved", node.Id)
 	}
 
+	// creating a new peer with given values
 	peer := tree.NewPeer(node)
 
-	network.addToNetwork(peer)
+	// add to the network
+	network.add(peer)
 
+	// update the new node id
 	network.ids[node.Id] = struct{}{}
 
 	return nil
 }
 
+// Leave: a node leaving the network
 func (network *P2PNetwork) Leave(id int) error {
+	// using locks to prevent from concurrent access
 	network.lock.Lock()
 	defer network.lock.Unlock()
 
+	// locate the peer and the tree for the given id
 	var peer *tree.Peer
 	var tree *tree.Tree
 
@@ -60,14 +79,20 @@ func (network *P2PNetwork) Leave(id int) error {
 		}
 	}
 
+	// if the given id is not in the topology, then return an error
 	if peer == nil {
 		return fmt.Errorf("cannot locate id %d node", id)
 	}
 
-	return network.removeFromNetwork(peer, tree)
+	// remove the peer from the network
+	network.remove(peer, tree)
+
+	return nil
 }
 
+// Trace: returns the current status of the network
 func (network *P2PNetwork) Trace() []string {
+	// using locks to prevent from concurrent access
 	network.lock.Lock()
 	defer network.lock.Unlock()
 
@@ -80,17 +105,23 @@ func (network *P2PNetwork) Trace() []string {
 	return digram
 }
 
-func (network *P2PNetwork) addToNetwork(peer *tree.Peer) {
+// add: adds the given peer to the network
+func (network *P2PNetwork) add(peer *tree.Peer) {
+	// get peer which has more capacity from the treap
 	parent := network.treap.Get()
 
+	// if there are no peers with enough capacity, then add the given peer into a new tree
 	if parent == nil {
-
+		// new peer will become the root of the tree. so parent should be nil
 		peer.SetParent(nil)
 
+		// create a new tree with the given peer
 		tree := tree.NewTree(peer)
 
+		// add the new tree into the network topology
 		network.topology = append(network.topology, tree)
 
+		// if the given peer has enough capacity, then insert it into the treap
 		if peer.Capacity > 0 {
 			network.treap.Insert(peer)
 		}
@@ -98,10 +129,13 @@ func (network *P2PNetwork) addToNetwork(peer *tree.Peer) {
 		return
 	}
 
+	// add the given peer into the children list of the parent peer which has more capacity
 	parent.AddChild(peer)
 
+	// update the parent peer in the treap by delete and re insert it
 	network.treap.Delete(parent.Id)
 
+	// only insert peers into the treap, if they have enough capacity
 	if parent.Capacity > 0 {
 		network.treap.Insert(parent)
 	}
@@ -111,100 +145,153 @@ func (network *P2PNetwork) addToNetwork(peer *tree.Peer) {
 	}
 }
 
-func (network *P2PNetwork) removeFromNetwork(peer *tree.Peer, tree *tree.Tree) error {
+// remove: removes the given peer from the network
+func (network *P2PNetwork) remove(peer *tree.Peer, tree *tree.Tree) {
 	parent := peer.Parent
 
+	// if the leaving peer is not the root, then remove the leaving peer from it's parent
 	if parent != nil {
 		parent.RemoveChild(peer)
 	}
 
+	// delete the leaving peer from ids map and treap
 	delete(network.ids, peer.Id)
-
 	network.treap.Delete(peer.Id)
 
+	// CASE A: removes a leaf peer
 	if len(peer.Children) == 0 {
 
+		// CASE A-1: the leaving peer is the root of the tree. need to delete the entire tree
 		if parent == nil {
 			network.removeTree(tree)
-			return nil
+			return
 		}
 
+		// CASE A-2: leaving peer is not the root of the tree
+
+		// update the parent peer in the treap
 		network.treap.Delete(parent.Id)
 		network.treap.Insert(parent)
 
+		// re arrange the parent in the tree
 		network.reArrange(parent, tree)
 
-		return nil
+		return
 	}
 
+	// CASE B: removes a peer which has exactly one child
 	if len(peer.Children) == 1 {
+		// next child would be the child of the leaving peer
 		nextChild := peer.Children[0]
 
+		// CASE B-1: the leaving peer is the root of the tree.
+		// need to set new child as the root of the tree
 		if parent == nil {
 			tree.SetRoot(nextChild)
 			nextChild.SetParent(nil)
-			return nil
+
+			return
 		}
 
+		// CASE B-2: the leaving peer is not the root of the tree.
+
+		// add the next child to the children list of the leaving peer's parent
 		parent.AddChild(nextChild)
 
+		// re arrange the next child in the tree
 		network.reArrange(nextChild, tree)
 
-		return nil
+		return
 	}
 
-	// sort the children according to most capacity
+	// CASE C: removes a peer which has more than one child
+
+	// sort the children according to capacity
 	sort.SliceStable(peer.Children, func(i, j int) bool {
 		return peer.Children[i].Capacity > peer.Children[j].Capacity
 	})
 
-	// next child would be, child which has most capacity
+	// next child would be the child which has most capacity
 	nextChild := peer.Children[0]
 
+	// if the leaving peer is the root, then next child become the root of the tree
 	if parent == nil {
 		tree.SetRoot(nextChild)
 		nextChild.SetParent(nil)
-	} else {
+	}
+
+	// if the leaving peer is not the root,
+	// then add the next child to children list of the parent peer of the leaving peer
+	if parent != nil {
 		parent.AddChild(nextChild)
 	}
 
+	// remaining children would be added to the network
 	for _, child := range peer.Children[1:] {
+		// delete child's tree peers from the treap
+		// to prevent from adding the child to it's own tree
 		network.treap.DeepDelete(child)
-		network.addToNetwork(child)
+
+		// add to the network
+		network.add(child)
+
+		// re insert the deleted child's tree peers
 		network.treap.DeepInsert(child)
 	}
 
+	// re arrange the next child in the tree
 	network.reArrange(nextChild, tree)
 
-	return nil
+	return
 }
 
-func (network *P2PNetwork) reArrange(peer *tree.Peer, t *tree.Tree) {
+// reArrange: recursively re arranges the given peer on the given tree
+// to make sure that the tree's depth would become smaller.
+// the peer moving up words based on it's capacity and it's parent capacity
+func (network *P2PNetwork) reArrange(peer *tree.Peer, tree *tree.Tree) {
 	if peer == nil || peer.Parent == nil {
 		return
 	}
 
-	canReArrange := peer.Capacity > (peer.Parent.Capacity + 1)
-
-	if !canReArrange {
+	// if the given peer has sufficient capacity (parent peer capacity +1) than it's parent peer,
+	// then re arrange the peer with it's parent (make the parent peer as child of the given peer)
+	if !(peer.Capacity > (peer.Parent.Capacity + 1)) {
 		return
 	}
 
 	parent := peer.Parent
 	grandParent := parent.Parent
 
+	/*
+		re arrangement
+
+			  1(1/1)
+				|		re arrange 10		     10(2/3)
+			  10(1/3)  ---------------->          /   \
+				|                             1(0/1) 12(0/2)
+			  12(0/2)
+	*/
+
+	// remove the given peer from the children list of the parent
 	parent.RemoveChild(peer)
 
+	// if the parent is root of the tree, then the given peer become the root of the tree
 	if grandParent == nil {
-		t.SetRoot(peer)
+		tree.SetRoot(peer)
 		peer.SetParent(nil)
-	} else {
+	}
+
+	// if the parent is not root of the tree,
+	// then add the given peer into the children list of it's grand parent
+	if grandParent != nil {
 		grandParent.RemoveChild(parent)
 		grandParent.AddChild(peer)
 	}
 
+	// add the parent to the children list of the given peer
 	peer.AddChild(parent)
 
+	// update the peer and it's parent in the treap
 	network.treap.Delete(parent.Id)
 	network.treap.Delete(peer.Id)
 
@@ -214,10 +301,11 @@ func (network *P2PNetwork) reArrange(peer *tree.Peer, t *tree.Tree) {
 		network.treap.Insert(peer)
 	}
 
-	network.reArrange(peer, t)
-	return
+	// keep doing
+	network.reArrange(peer, tree)
 }
 
+// removeTree: removes the given tree from the network topology
 func (network *P2PNetwork) removeTree(t *tree.Tree) {
 	topology := make([]*tree.Tree, 0)
 
